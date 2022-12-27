1ebf547764042d7a442e505df14b6050e8fefbfa
Tom Monks <monksy@gmail.com>
2022-12-26 20:06:21 -0500

    reset_pattern function and cleanup

--- RPS.py
+++ RPS.py
@@ -1,79 +1,38 @@
 import random
-# The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
 
 
-# def player(prev_play, opponent_history=[], counter=[0], pattern=['R', 'P', 'S']):
 def player(prev_play, opponent_history=[], counter=[0], pattern=[]):
-    # reset counter if starting a new player
+    # reset counter and pattern if starting a new player
     if prev_play == '':
-        # print('Resetting counter')
-        pattern.clear()
-        pattern.append('S')
-        pattern.append('S')
-        pattern.append('P')
-        pattern.append('S')
-        pattern.append('R')
-        pattern.append('P')
-        pattern.append('P')
-        pattern.append('R')
-        pattern.append('R')
-        # pattern = ['S', 'S', 'P', 'S', 'R', 'P', 'P', 'R', 'R']
+        reset_pattern(pattern, 'SSPSRPPRR')
         counter[0] = 0
         opponent_history.clear()
 
     counter[0] += 1
 
+    # keep track of the opponent's first two plays
     if prev_play != '' and len(opponent_history) < 2:
-        # print(f'appending prev_play: {prev_play}')
         opponent_history.append(prev_play)
 
-    # print(f'opponent_history: {opponent_history}')
-    # print(f'using pattern: {pattern}')
-
+    # quincy starts with RP, mrugesh with RR, kris with PR, abbey with PP
     if counter[0] == 3:
         first_two = "".join(opponent_history)
         if first_two == 'RP':
             print('Playing quincy')
-            pattern.clear()
-            # RRPPS
-            pattern.append('P')
-            pattern.append('P')
-            pattern.append('S')
-            pattern.append('S')
-            pattern.append('R')
+            reset_pattern(pattern, 'PPSSR')
         elif first_two == 'RR':
             print('Playing mrugesh')
-            pattern.clear()
-            pattern.append('R')
-            pattern.append('S')
-            pattern.append('P')
-            # pattern = ['R', 'P', 'S']
+            reset_pattern(pattern, 'RSP')
         elif first_two == 'PR':
             print('Playing kris')
-            pattern.clear()
-            pattern.append('R')
-            pattern.append('S')
-            pattern.append('P')
-            # pattern = ['R', 'P', 'S']
-        elif first_two == 'PP':
-            print('Playing abbey')
-            # pattern = ['S', 'S', 'P' 'S', 'R', 'P', 'P', 'R', 'R']
+            reset_pattern(pattern, 'RSP')
         else:
             print('Playing unknown')
 
     return pattern[counter[0] % len(pattern)]
 
 
-def abbey_player(prev_play, counter=[0]):
-
-    counter[0] += 1
-    choices = [x for x in 'SSPSRPPRR']
-    return choices[counter[0] % len(choices)]
-
-
-def rps_player(prev_play, counter=[0]):
-
-    counter[0] += 1
-    # choices = ['S', 'P', 'R']
-    choices = ['R', 'S', 'P']
-    return choices[counter[0] % len(choices)]
+def reset_pattern(list, pattern):
+    list.clear()
+    for letter in pattern:
+        list.append(letter)
