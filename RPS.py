a0ef0a78bb571d011766dc994eed8220011e777f
Tom Monks <monksy@gmail.com>
2022-12-28 10:33:50 -0500

    Clean up debugging, add .gitignore, enable tests

--- RPS.py
+++ RPS.py
@@ -4,6 +4,7 @@ import random
 def player(prev_play, opponent_history=[], counter=[0], pattern=[]):
     # reset counter and pattern if starting a new player
     if prev_play == '':
+        # default pattern, tailored for abbey
         reset_pattern(pattern, 'SSPSRPPRR')
         counter[0] = 0
         opponent_history.clear()
@@ -17,22 +18,18 @@ def player(prev_play, opponent_history=[], counter=[0], pattern=[]):
     # quincy starts with RP, mrugesh with RR, kris with PR, abbey with PP
     if counter[0] == 3:
         first_two = "".join(opponent_history)
-        if first_two == 'RP':
-            print('Playing quincy')
+        if first_two == 'RP':  # quincy
             reset_pattern(pattern, 'PPSSR')
-        elif first_two == 'RR':
-            print('Playing mrugesh')
+        elif first_two == 'RR':  # mrugesh
             reset_pattern(pattern, 'RSP')
-        elif first_two == 'PR':
-            print('Playing kris')
+        elif first_two == 'PR':  # kris
             reset_pattern(pattern, 'RSP')
-        else:
-            print('Playing unknown')
 
     return pattern[counter[0] % len(pattern)]
 
 
-def reset_pattern(list, pattern):
-    list.clear()
-    for letter in pattern:
+def reset_pattern(pattern, string):
+    """ resets pattern to a list of characters in string """
+    pattern.clear()
+    for letter in string:
         list.append(letter)
--- main.py
+++ main.py
@@ -3,10 +3,11 @@ from RPS_game import play, mrugesh, abbey, quincy, kris, human, random_player
 from RPS import player
 from unittest import main
 
-play(player, quincy, 1000)
-play(player, abbey, 1000)
-play(player, kris, 1000)
-play(player, mrugesh, 1000)
+# Uncomment these lines for testing
+# play(player, quincy, 1000)
+# play(player, abbey, 1000)
+# play(player, kris, 1000)
+# play(player, mrugesh, 1000)
 
 # Uncomment line below to play interactively against a bot:
 # play(human, abbey, 20, verbose=True)
@@ -15,6 +16,5 @@ play(player, mrugesh, 1000)
 # play(human, random_player, 1000)
 
 
-
 # Uncomment line below to run unit tests automatically
-# main(module='test_module', exit=False)
+main(module='test_module', exit=False)
